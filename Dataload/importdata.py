001d11b1156630f610506e2e6c488cce46457d8f
Jeeva-G <jeeva.analyst@gmail.com>
2016-05-10 16:01:07 +1200

    updated data load file.

--- Dataload/importdata.py
+++ Dataload/importdata.py
@@ -1,10 +1,7 @@
 # Reading article details from input text file.
 """This script reads the text file and make a python list out of it."""
-import pandas as pd
 
 articlelist = []
 with open("/Users/jeevananthamganesan/Documents/\
 Inputdata_github/Article.txt") as f:
     data = f.readlines()
-
-articledata = pd.DataFrame(articlelist)
